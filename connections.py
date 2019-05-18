17710a1cf76d0d860bde35a392ed8f79526e65f3
ajin sanu <ajinsanut@gmail.com>
2019-05-18 17:29:40 +0530

    update

--- connections.py
+++ connections.py
@@ -8,7 +8,7 @@ DATABASE_MAP = {
 
 
 def get_connection_to(db_name='dash'):
-    conn = MySQLdb.connect(host="localhost", user="root", passwd="password", port=3306, db=DATABASE_MAP[db_name.upper()])
+    conn = MySQLdb.connect(host="localhost", user="root", passwd="sh@d0w", port=3306, db=DATABASE_MAP[db_name.upper()])
     cursor = conn.cursor()
     return cursor, conn
 
